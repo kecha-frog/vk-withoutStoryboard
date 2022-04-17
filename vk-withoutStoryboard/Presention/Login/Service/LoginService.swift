49296aeed74f8be99948b182dc452256f25eec7e
Ke4a <helpomos@gmail.com>
2022-04-17 16:31:00 +0500

    Big update: refactoring code add comment add doc

---
+++ vk-withoutStoryboard/Presention/Login/Service/LoginService.swift
@@ -0,0 +1,22 @@
+//
+//  LoginService.swift
+//  vk-withoutStoryboard
+//
+//  Created by Ke4a on 05.04.2022.
+//
+
+import Foundation
+//import Firebase
+
+/// Сервисный слой для LoginViewController.
+class LoginService{
+    //private let ref: DatabaseReference = Database.database().reference(withPath: "Authorized_Id")
+    
+    /// Отправка  id авторизированных пользователей в firebase.
+    /// - Parameter id: Autorized user id.
+//    func firebaseAutorizedId(_ id: String){
+//        let autorizedId: LoginAutorized = LoginAutorized(id)
+//        let autorizedRef: DatabaseReference = self.ref.child(autorizedId.userId)
+//        autorizedRef.setValue(autorizedId.toAnyObject())
+//    }
+}
