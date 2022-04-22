c2943f2a4298237a185ff56bde523fd0eea18e30
Ke4a <helpomos@gmail.com>
2022-04-22 16:52:43 +0500

    Multithreaded

--- vk-withoutStoryboard/CommonFiles/NetworkService/ApiVK.swift
+++ vk-withoutStoryboard/CommonFiles/NetworkService/ApiVK.swift
@@ -26,6 +26,7 @@ extension ApiVK{
         case searchGroup = "/method/groups.search"
         case getCatalogGroups = "/method/groups.getCatalog"
         case getNews = "/method/newsfeed.get"
+        case getUser = "/method/users.get"
     }
 }
 
@@ -53,15 +54,8 @@ final class ApiVK{
     }
     
     private init(){}
-    
-    /// Запрос на сервер Api.
-    /// - Parameters:
-    ///   - model: Объект декодирования ответа сервера
-    ///   - method: Метод запроса GET или POST.
-    ///   - path: путь к api
-    ///   - params: параметры запроса
-    ///   - completion: Замыкание. Передает: `Result` c декодированным ответом  сервера или ошибку.
-    func reguest<T: ModelApiVK>(_ model: T.Type, method: Method, path: Path, params: [String:String]?, completion: @escaping (Result<JSONResponse<T>, ServiceError>) -> Void) {
+
+    private func _requestPrivate(method: Method, path: Path, params: [String:String]?, completion: @escaping (Result<Data, ServiceError>) -> Void) {
         var localParams: [URLQueryItem] = self.params
         if (params != nil){
             params?.forEach({ (key, value) in
@@ -91,67 +85,159 @@ final class ApiVK{
                 return
             }
             
-            do {
-                // декодирование ответа сервера
-                let codableData: JSONResponse<T> = try JSONDecoder().decode(JSONResponse<T>.self, from: validData)
-                
-                DispatchQueue.main.async {
-                    completion(.success(codableData))
-                }
-            }catch {
-                DispatchQueue.main.async {
-                    completion(.failure(.parseError))
-                    print(error)
-                }
+            DispatchQueue.main.async{
+                completion(.success(validData))
             }
         }
         task.resume()
     }
     
-    /// Проверка токена на валидность с помощью Api.
-    /// - Parameter completion: Замыкание.  Передает:  bool - исходя из ответа сервера.
-    func checkToken( _ completion : @escaping (Bool) -> Void ){
-        var localUrlComponents: URLComponents = self.urlComponents
-        
-        var localParams: [URLQueryItem] = self.params
-        localParams.append(.init(name: "access_token", value: token))
-        
-        localUrlComponents.path = "/method/users.get"
-        localUrlComponents.queryItems = localParams
-        
-        guard let url: URL = localUrlComponents.url else {
-            completion(false)
-            return
-        }
-        
-        var request: URLRequest = URLRequest(url: url)
-        request.httpMethod = Method.GET.rawValue
-        
-        let task: URLSessionDataTask = httpSession.dataTask(with: request) { (data, response, error) in
-            guard let validData: Data = data, error == nil else {
-                DispatchQueue.main.async{
-                    completion(false)
-                }
-                return
-            }
-            
-            do {
-                let json: [String:Any]? = try JSONSerialization.jsonObject(with: validData, options: .mutableContainers) as? [String: Any]
-                
-                DispatchQueue.main.async{
-                    //проверка рабочий ли токен (придёт response или error)
-                    if let result: Bool = json?.keys.contains("response"){
-                        completion(result)
-                    }else{
-                        completion(false)
+    /// Запрос на сервер Api.
+    /// - Parameters:
+    ///   - model: Объект декодирования ответа сервера
+    ///   - method: Метод запроса GET или POST.
+    ///   - path: путь к api
+    ///   - params: параметры запроса
+    ///   - completion: Замыкание. Передает: `Result` c декодированным ответом  сервера или ошибку.
+    func request<T: ModelApiVK>(_ model: T.Type, method: Method, path: Path, params: [String:String]?, completion: @escaping (Result<JSONResponse<T>, ServiceError>) -> Void) {
+        _requestPrivate(method: method, path: path, params: params) { result in
+            switch result{
+            case .success(let data):
+                do {
+                    // декодирование ответа сервера
+                    let response: JSONResponse<T> = try JSONDecoder().decode(JSONResponse<T>.self, from: data)
+                    
+                    DispatchQueue.main.async {
+                        completion(.success(response))
+                    }
+                }catch {
+                    DispatchQueue.main.async {
+                        completion(.failure(.parseError))
+                        print(error)
                     }
                 }
-            }catch {
+            case.failure(let error):
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                    print(error)
+                }
+            }
+        }
+    }
+    
+    /// Запрос списка на сервер Api.
+    /// - Parameters:
+    ///   - model: Объект декодирования ответа сервера
+    ///   - method: Метод запроса GET или POST.
+    ///   - path: путь к api
+    ///   - params: параметры запроса
+    ///   - completion: Замыкание. Передает: `Result` c декодированным ответом  сервера или ошибку.
+    func requestItems<T: ModelApiVK>(_ model: T.Type, method: Method, path: Path, params: [String:String]?, completion: @escaping (Result<JSONResponseItems<T>, ServiceError>) -> Void) {
+        _requestPrivate(method: method, path: path, params: params) { result in
+            switch result{
+            case .success(let data):
+                let dispatchGroup = DispatchGroup()
+                let decoder = JSONDecoder()
+                
+                do{
+                    let json: [String:Any]? = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any]
+                    let responseJson: [String:Any]? = json?["response"] as? [String : Any]
+                    guard let responseJson = responseJson else { return}
+                    
+                    var items: [T]?
+                    var count: Int?
+                    var profiles: [NewsProfileModel]?
+                    var groups: [NewsGroupModel]?
+                    
+                    // Многопоточный парсинг данных.
+                    DispatchQueue.global().async(group: dispatchGroup){
+                        
+                        do {
+                            let itemsData = try JSONSerialization.data(withJSONObject: responseJson["items"])
+                            items = try decoder.decode([T].self, from: itemsData)
+                            
+                            count = responseJson["count"] as? Int
+                        }catch {
+                            completion(.failure(.parseError))
+                            print(error)
+                        }
+                        
+                    }
+                    
+                    // Если запрос новости, то дополнительно приходят профили и группы
+                    if model == NewsPostModel.self{
+                        // Многопоточный парсинг данных.
+                        DispatchQueue.global().async(group: dispatchGroup){
+                            do {
+                                let profilesData = try JSONSerialization.data(withJSONObject: responseJson["profiles"])
+                                profiles = try decoder.decode([NewsProfileModel].self, from: profilesData)
+                            }catch {
+                                completion(.failure(.parseError))
+                                print(error)
+                            }
+                            
+                        }
+                        // Многопоточный парсинг данных.
+                        DispatchQueue.global().async(group: dispatchGroup){
+                            do {
+                                let groupsData = try JSONSerialization.data(withJSONObject: responseJson["groups"])
+                                groups = try decoder.decode([NewsGroupModel].self, from: groupsData)
+                            }catch {
+                                completion(.failure(.parseError))
+                                print(error)
+                            }
+                            
+                        }
+                    }
+                    
+                    dispatchGroup.notify(queue: DispatchQueue.main){
+                        guard let items = items else { return }
+                        
+                        let responseApi = JSONResponseItems(items, count, profiles, groups)
+                        completion(.success(responseApi))
+                    }
+                    
+                }catch{
+                    DispatchQueue.main.async {
+                        completion(.failure(.parseError))
+                        print(error)
+                    }
+                }
+            case.failure(let error):
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                    print(error)
+                }
+            }
+        }
+    }
+    
+    
+    /// Проверка токена на валидность с помощью Api.
+    /// - Parameter completion: Замыкание.  Передает:  bool - исходя из ответа сервера.
+    func requestCheckToken( _ completion : @escaping (Bool) -> Void ){
+        _requestPrivate(method: .GET, path: .getUser, params: nil) { result in
+            switch result{
+            case .success(let data):
+                do {
+                    let json: [String:Any]? = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any]
+                    
+                    DispatchQueue.main.async{
+                        //проверка рабочий ли токен (придёт response или error)
+                        if let result: Bool = json?.keys.contains("response"){
+                            completion(result)
+                        }else{
+                            completion(false)
+                        }
+                    }
+                }catch {
+                    
+                }
+            case .failure(_):
                 DispatchQueue.main.async {
                     completion(false)
                 }
             }
         }
-        task.resume()
     }
 }
--- vk-withoutStoryboard/Presention/TabBarContoller/Items/NewsList/Service/NewsService.swift
+++ vk-withoutStoryboard/Presention/TabBarContoller/Items/NewsList/Service/NewsService.swift
@@ -28,12 +28,14 @@ class NewsService {
     /// Запрос из api новостей.
     /// - Parameter completion: Замыкание.
     func fetchApiAsync(_ completion: @escaping ()-> Void){
-        ApiVK.standart.reguest(PostModel.self, method: .POST, path: .getNews, params: ["filters": "post"]) { result in
+        ApiVK.standart.requestItems(NewsPostModel.self, method: .POST, path: .getNews, params: ["filters": "post"]) { result in
             switch result {
             case .success(let response):
                 // Преобразовываем данные с новостями для работы с таблицей.
-                self.getValidDate(response)
-                completion()
+                self.getValidDate(response){ news in
+                    self.data = news
+                    completion()
+                }
             case .failure(let error):
                 print(error)
                 completion()
@@ -45,68 +47,93 @@ class NewsService {
     /// - Parameter response: Ответ сервера с вспомогательными данными.
     ///
     ///  Собирает данные для секции новости.
-    private func getValidDate(_ response: JSONResponse<PostModel>){
-        // Массив секций с новостями
-        var validData:[[postType]] = []
+    private func getValidDate(_ response: JSONResponseItems<NewsPostModel>, _ completion: @escaping (_ news: [[postType]] )-> Void){
+        var profiles: [Int: NewsProfileModel]?
+        var groups: [Int: NewsGroupModel]?
         
-        response.items.forEach { news in
-            // Секция с новостью
-            var validNews: [postType] = []
-            
-            // Получаем профиль группы или юзера для хедера.
-            // Отправляем в секцию
-            if news.sourceId > 0,
-               let profile = response.profiles?.first(where: { $0.id == news.sourceId}) {
-                validNews.append(.profile(profile, news.date))
-            }else if
-                let group = response.groups?.first(where: { $0.id == abs(news.sourceId)}) {
-                validNews.append(.group(group, news.date))
-            }
-            
-            // Контент новости
-            var photos: [Attachment] = []
-            var audio: [Attachment] = []
-            var video: [Attachment] = []
-            var other: [Attachment] = []
-            // Сортируем контент и добавляем
-            news.attachments?.forEach{ content in
-                if content.type == "photo" {
-                    photos.append(content)
-                }else if content.type == "audio" {
-                    audio.append(content)
-                }else if content.type == "video" {
-                    video.append(content)
-                }else{
-                    other.append(content)
-                }
-            }
-            
-            // Добавляем контент новости в секцию
-            if !photos.isEmpty{
-                validNews.append(.photo(photos))
-            }
-            if !audio.isEmpty{
-                validNews.append(.audio(audio))
-            }
-            if !video.isEmpty{
-                validNews.append(.video(video))
-            }
-            if !other.isEmpty{
-                validNews.append(.other(other))
-            }
-            
-            // Добавляем текст новости в секцию
-            if news.text != ""{
-                validNews.append(.text(news.text))
-            }
-            
-            // Добавляем футер секции
-            validNews.append(.likeAndView(news.likes, news.views, news.comments))
-            
-            // Добавляем полученную секцию в список
-            validData.append(validNews)
+        // Многопоточный парсинг данных.
+        let dispatchGroup = DispatchGroup()
+        DispatchQueue.global().async(group: dispatchGroup){
+            profiles = response.helpers?.profiles?.reduce([Int: NewsProfileModel](), { partialResult, profile in
+                var value = partialResult
+                value[profile.id] = profile
+                return value
+            }) ?? [:]
         }
         
-        self.data = validData
+        DispatchQueue.global().async(group: dispatchGroup){
+            groups = response.helpers?.groups?.reduce([Int: NewsGroupModel](), { partialResult, groups in
+                var value = partialResult
+                value[groups.id] = groups
+                return value
+            }) ?? [:]
+        }
+        
+        
+        dispatchGroup.notify(queue: DispatchQueue.main){
+            // Массив секций с новостями
+            var validData:[[postType]] = []
+            
+            response.items.forEach { news in
+                
+                // Секция с новостью
+                var validNews: [postType] = []
+                
+                // Получаем профиль группы или юзера для хедера.
+                // Отправляем в секцию
+                if news.sourceId > 0,
+                   let profile = profiles?[news.sourceId] {
+                    validNews.append(.profile(profile, news.date))
+                }else if
+                    let group = groups?[abs(news.sourceId)] {
+                    validNews.append(.group(group, news.date))
+                }
+                
+                // Контент новости
+                var photos: [Attachment] = []
+                var audio: [Attachment] = []
+                var video: [Attachment] = []
+                var other: [Attachment] = []
+                // Сортируем контент и добавляем
+                news.attachments?.forEach{ content in
+                    if content.type == "photo" {
+                        photos.append(content)
+                    }else if content.type == "audio" {
+                        audio.append(content)
+                    }else if content.type == "video" {
+                        video.append(content)
+                    }else{
+                        other.append(content)
+                    }
+                }
+                
+                // Добавляем контент новости в секцию
+                if !photos.isEmpty{
+                    validNews.append(.photo(photos))
+                }
+                if !audio.isEmpty{
+                    validNews.append(.audio(audio))
+                }
+                if !video.isEmpty{
+                    validNews.append(.video(video))
+                }
+                if !other.isEmpty{
+                    validNews.append(.other(other))
+                }
+                
+                // Добавляем текст новости в секцию
+                if news.text != ""{
+                    validNews.append(.text(news.text))
+                }
+                
+                // Добавляем футер секции
+                validNews.append(.likeAndView(news.likes, news.views, news.comments))
+                
+                // Добавляем полученную секцию в список
+                validData.append(validNews)
+            }
+            
+            completion(validData)
+        }
     }
 }
